08dda3fb4480e1eee21bd8af1e300d73f7b39cd6
Ajaybisht13 <ajaybisht203@gmail.com>
2022-05-08 12:05:01 +0530

    update

---
+++ src/config/api.js
@@ -0,0 +1,10 @@
+export const API = 'http://grocerybuy.store/';
+
+export const CONSUMER_KEY = 'ck_42fcd2e1a6c5756d232253b735326521dc91ca61';
+export const CONSUMER_SECRET = 'cs_bce5ac23928f33dee3ddff7d3127553dd9ab9f20';
+
+export default {
+  API_ENDPOINT: API,
+  CONSUMER_KEY,
+  CONSUMER_SECRET,
+};
